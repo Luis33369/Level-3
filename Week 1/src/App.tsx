fdfddaae0f1259fdd6b69a96f7ec8b2706615488
luis33369 <luis33369@icloud.com>
2025-12-20 14:02:47 -0800

    a

--- Week 1/src/App.tsx
+++ Week 1/src/App.tsx
@@ -23,6 +23,7 @@ function App() {
         <HeaderWithLinks img="https://cdn.doordash.com/media/consumer/home/landing/new/ScootScoot.svg" title="Become a Dasher" paragraph="As a delivery driver, make monery and work on your schedule. Sign up in minutes." link="https://www.doordash.com/" link2="Start earning"/>
         <HeaderWithLinks img="https://cdn.doordash.com/media/consumer/home/landing/new/Storefront.svg" title='Become a merchant' paragraph='Attract new customer and grow sales, starting with 0% commissions for up to 30 days.' link='https://www.doordash.com/'  link2='Sign up for Doordash'/>
         <HeaderWithLinks img='https://cdn.doordash.com/media/consumer/home/landing/new/iphone.svg' title="Get the best DoorDash experience" paragraph="Experience the best your neighborhood has to offer, all in one app." link="ttps://www.doordash.com/" link2="Get the app" />
+  
       </div>
       {/* Footer */}
       <Footer />
