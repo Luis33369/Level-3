255d39ed02fd5492be44edec5c782e7a25760876
Luis33369 <48746512+Luis33369@users.noreply.github.com>
2025-12-22 23:09:53 -0800

    first part done

--- Week 1/src/App.tsx
+++ Week 1/src/App.tsx
@@ -4,11 +4,10 @@ import viteLogo from './assets/vite.svg';
 // import hero_desktop from './assets/hero_desktop.png';
 import './App.css'
 
-import { Body } from './components/body';
+// import { Body } from './components/body';
 import { Footer } from './components/footer';
 import { HeaderWithLinks } from './components/body';
 import { Navbar } from "./components/header";
-
 
 
 
@@ -17,13 +16,13 @@ function App() {
   return (
     <>
       {/* Header */}
-      <Navbar/>
+      <Navbar />
       {/* Body */}
       <div className='flex justify-between w-full'>
-        <HeaderWithLinks img="https://cdn.doordash.com/media/consumer/home/landing/new/ScootScoot.svg" title="Become a Dasher" paragraph="As a delivery driver, make monery and work on your schedule. Sign up in minutes." link="https://www.doordash.com/" link2="Start earning"/>
-        <HeaderWithLinks img="https://cdn.doordash.com/media/consumer/home/landing/new/Storefront.svg" title='Become a merchant' paragraph='Attract new customer and grow sales, starting with 0% commissions for up to 30 days.' link='https://www.doordash.com/'  link2='Sign up for Doordash'/>
+        <HeaderWithLinks img="https://cdn.doordash.com/media/consumer/home/landing/new/ScootScoot.svg" title="Become a Dasher" paragraph="As a delivery driver, make monery and work on your schedule. Sign up in minutes." link="https://www.doordash.com/" link2="Start earning" />
+        <HeaderWithLinks img="https://cdn.doordash.com/media/consumer/home/landing/new/Storefront.svg" title='Become a merchant' paragraph='Attract new customer and grow sales, starting with 0% commissions for up to 30 days.' link='https://www.doordash.com/' link2='Sign up for Doordash' />
         <HeaderWithLinks img='https://cdn.doordash.com/media/consumer/home/landing/new/iphone.svg' title="Get the best DoorDash experience" paragraph="Experience the best your neighborhood has to offer, all in one app." link="ttps://www.doordash.com/" link2="Get the app" />
-  
+
       </div>
       {/* Footer */}
       <Footer />
--- Week 1/src/components/body.tsx
+++ Week 1/src/components/body.tsx
@@ -13,7 +13,7 @@ interface HeaderWithLinks {
 
 export const HeaderWithLinks = ({ title, paragraph, link, img, link2 }: HeaderWithLinks) => {
   return (
-    <div className="text-gray-100 flex flex-col flex-wrap items-center justify-between h p-20 gap-2"> 
+    <div className="text-gray-100 flex flex-col flex-wrap items-center justify-between h p-20 gap-2">
       <img className="w-64" src={img} alt="" />
       <h2 className="text-3xl">{title}</h2>
       <div className="w-60">{paragraph}</div>
@@ -23,22 +23,24 @@ export const HeaderWithLinks = ({ title, paragraph, link, img, link2 }: HeaderWi
   );
 };
 
-export const Body = () => {
-  const [count, setCount] = useState<number>(0);
+// export const Body = () => {
+//   const [count, setCount] = useState<number>(0);
+//
+//   return (
+//     <>
+//       <h1></h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to test HMR
+//         </p>
+//       </div>
+//     </>
+//   );
+// }
 
-  return (
-    <>
-      <h1></h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
-  );
-}
+
 
 
